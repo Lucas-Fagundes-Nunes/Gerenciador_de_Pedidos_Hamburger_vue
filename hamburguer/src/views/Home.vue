1ae6696d0ac60cc3367443bc2244462f4414da7a
Lucas-Fagundes-Nunes <113182658+Lucas-Fagundes-Nunes@users.noreply.github.com>
2022-11-18 08:12:07 -0300

    Add files via upload

---
+++ hamburguer/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu Hamburger:</h1>
+    <Formulario />
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import Formulario from "../components/Formulario.vue"
+
+export default {
+  name:"Home",
+  components: {
+    Banner,
+    Formulario
+  }
+}
+</script>
